09f4331c5b70fae5ee9ebd5e9a34c3eab5bd55d4
bowmanpete <mesa123123@users.noreply.github.com>
2022-01-31 01:37:05 +1300

    Worked futher on general solving for hidden pairs+
    Tried adding a frequency method and reverse possibilities in the solver
    class but its slow going will continue

--- src/typescript/solver.ts
+++ src/typescript/solver.ts
@@ -2,6 +2,28 @@ import { cell } from "./cell";
 import { grid } from "./grid";
 
 export class Solver {
+  // Remove all unnecessary pencil marks from all houses, this is quite computationally expensive if we do this after every change, could we just do this to the houses affected by the changed cell? @todo
+  cleanPencilMarks(grid: grid): grid {
+    grid.cells.forEach((row) => {
+      row.forEach((cell) => {
+        // Get the rows and columns where the cell could be affected, filter them out so only those with values remain
+        let dependentCellsWithValue: Array<cell> = grid
+          .getHouses(cell.getCoordinates(), grid.cells)
+          .flat()
+          .filter((cell) => cell.value != 0);
+        let notPossibleNumbers: Array<number> = dependentCellsWithValue.map(
+          (cell) => {
+            return cell.value;
+          }
+        );
+        cell.possible = cell.possible.filter(
+          (value) => !notPossibleNumbers.includes(value)
+        );
+      });
+    });
+    return grid;
+  }
+
   // Solve any lone singles that are sitting about in the mix
   loneSingles(grid: Array<Array<cell>>): Array<Array<cell>> {
     grid.forEach((gridRow) => {
@@ -13,28 +35,6 @@ export class Solver {
       });
     });
     return grid;
-  }
-
-  // Remove all unnecessary pencil marks from all houses, this is quite computationally expensive if we do this after every change, could we just do this to the houses affected by the changed cell? @todo
-  cleanPencilMarks(grid: grid): grid {
-    grid.cells.forEach((row) => {
-      row.forEach((cell) => {
-        // Get the rows and columns where the cell could be affected, filter them out so only those with values remain
-        let dependentCellsWithValue: Array<cell> = grid
-          .getHouses(cell.getCoordinates(), grid.cells)
-          .flat()
-          .filter((cell) => cell.value != 0 );
-        let notPossibleNumbers: Array<number> = dependentCellsWithValue.map(
-          (cell) => {
-            return cell.value;
-          }
-        );
-        cell.possible = cell.possible.filter(
-          (value) => !notPossibleNumbers.includes(value)
-        );
-      });
-    });
-    return grid
   }
 
   // Solve any hidden singles that are about in the grid
@@ -66,11 +66,14 @@ export class Solver {
     return grid.cells;
   }
 
-  // Finding hidden n > 1s is going to be tricky, basically we have a cell with n possible numbers and we need to find out if those possible numbers are occuring as a group
+  // This function returns a table of which cells in the house (arbitrarily numbered 1-9) each pencil mark occurs within
   getReversePossibilities(house: Array<cell>): Map<number, Array<number>> {
     //Create a Map that will get frequencies of all of the cells
-    let reversePossibilities: Map<number, Array<number>> = new Map<number, Array<number>>();
-    // Each key has a number between 1 and 9 given its position in the house (this is arbitrary but ordering them helps the algorithm
+    let reversePossibilities: Map<number, Array<number>> = new Map<
+      number,
+      Array<number>
+    >();
+    // Each key has a number between 1 and 9 given its position in the house (this is arbitrary but ordering them helps the algorithm)
     for (let i = 1; i < 10; i++) {
       reversePossibilities[i] = new Array<number>();
     }
@@ -87,12 +90,16 @@ export class Solver {
   }
 
   //Need an algorithm to find out if the n numbers in a houseFrequencyChart, are occuring in n number of cells, I'll call this a grouping of cells
-  checkForCellGrouping(
-    reversePossibilities: Map<number, Array<number>>,
-    house: Array<cell>
-  ): void {
-    let rPossibilitiesLengths: Array<number> = new Array<number>();
-    reversePossibilities.forEach((value) => { rPossibilitiesLengths.push(value.length) })
-     
+  checkForCellGrouping(house: Array<cell>): void {
+    // get the frequency table for the house
+    let reversePossibilities = this.getReversePossibilities(house);
+    // check the frequency table for numbers that are pencil marks in the same number of cells, i.e. get a frequency of frequencies
+    let frequencyTable: Map<number, number> = new Map<number, number>();
+    // This is dogshit! What the fuck am I even thinking, have I even analysed this problem at all! Fuck...
+    reversePossibilities.forEach((key) => {
+      frequencyTable[key.length] =
+        frequencyTable[key.length] == NaN ? 1 : frequencyTable[key.length] + 1;
+    });
+    // now find out if there are any keys and values in their frequency table that have that length or higher
   }
 }
